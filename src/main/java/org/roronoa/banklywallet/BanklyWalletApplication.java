ca3d54c3d41fa02d137571e2f122f0f024776fcb
Oussama Souayri <souayri.oussama@gmail.com>
2023-02-07 11:38:57 +0100

    init commit

---
+++ src/main/java/org/roronoa/banklywallet/BanklyWalletApplication.java
@@ -0,0 +1,13 @@
+package org.roronoa.banklywallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanklyWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanklyWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/roronoa/banklywallet/BanklyWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package org.roronoa.banklywallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklyWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
